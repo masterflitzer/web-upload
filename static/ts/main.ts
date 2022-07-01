f1db34c13d4d8295bb2b6d6b45277d944a7cd7d4
Masterflitzer <60170948+Masterflitzer@users.noreply.github.com>
2022-07-01 13:46:33 +0200

    update css, animations and logic

--- static/ts/main.ts
+++ static/ts/main.ts
@@ -4,35 +4,68 @@ colorScheme();
 
 const form: HTMLFormElement | null = document.querySelector("#form");
 const fileInput: HTMLInputElement | null = document.querySelector("#files");
+const submitButton: HTMLButtonElement | null =
+    document.querySelector("#submit");
 const alertContainer: HTMLSpanElement | null =
     document.querySelector("#alertContainer");
 const alertMessage: HTMLSpanElement | null =
     document.querySelector("#alertMessage");
 
+function toggleChildrenDisplay(element: Element, display: boolean) {
+    Array.from(element.children).forEach((e) => {
+        try {
+            const html = e as HTMLElement;
+            html.style.display = display ? "" : "none";
+        } catch (error) {
+            console.warn(error);
+        }
+    });
+}
+
 form?.addEventListener("submit", async (event) => {
     event.preventDefault();
-    const formData = new FormData(form);
 
+    if (
+        fileInput == null ||
+        submitButton == null ||
+        alertMessage == null ||
+        alertContainer == null
+    ) {
+        console.error("Couldn't find necessary elements");
+        return;
+    }
+
+    const animation = document.createElement("div");
+    animation.classList.add("dots-animation");
+    for (let i = 0; i < 3; i++) {
+        animation.append(document.createElement("span"));
+    }
+
+    toggleChildrenDisplay(submitButton, false);
+    submitButton.append(animation);
+
+    const formData = new FormData(form);
     const response = await fetch("/", {
         method: "post",
         body: formData,
     });
     const json = await response.json();
 
-    if (fileInput) {
-        fileInput.value = "";
+    fileInput.value = "";
+    fileInput.focus();
+
+    animation.remove();
+    toggleChildrenDisplay(submitButton, true);
+
+    if (json.success) {
+        alertMessage.textContent = "File(s) uploaded successfully!";
+    } else {
+        alertMessage.textContent = "File upload failed!";
     }
 
-    if (alertMessage && alertContainer) {
-        if (json.success) {
-            alertMessage.textContent = "File(s) uploaded successfully!";
-        } else {
-            alertMessage.textContent = "File upload failed!";
-        }
-        alertContainer.classList.remove("d-none");
-        setTimeout(() => {
-            alertContainer.classList.add("d-none");
-            alertMessage.textContent = null;
-        }, 5000);
-    }
+    alertContainer.classList.remove("d-none");
+    setTimeout(() => {
+        alertContainer.classList.add("d-none");
+        alertMessage.textContent = null;
+    }, 5000);
 });
